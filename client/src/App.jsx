6b6663937442e6a4dcc1337a7fe1c1de62500c80
Dikshantashresth <shresthadikshanta25@gmail.com>
2025-07-23 15:38:50 +0545

    v1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import axios from 'axios'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Home from './components/Home'
+import React from "react";
+import axios from "axios";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Register from "./components/Register";
 const App = () => {
   const routes = createBrowserRouter([
     {
-      path:'/',
-      element: <Home/>
-    }
-  ])
-  return (
-    <RouterProvider/>
-  )
-}
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/logout",
+      element: <Logout />,
+    },
+    {
+      path: "/chat/:id",
+    },
+    {
+      path: "/dashboard/:userid",
+      element: <Dashboard />,
+    },
+    {},
+  ]);
+  return <RouterProvider route={routes} />;
+};
 
-export default App
+export default App;
